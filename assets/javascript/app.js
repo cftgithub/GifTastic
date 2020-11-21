74c3767d8cc40ecd70465ec198fa8d2b9bb2248e
cftgithub <chiafongt@gmail.com>
2020-11-20 18:32:16 -0700

    modified search to random, 5 time per click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,6 @@
-$(document).ready(function () {
+// API from Giphy Developers
 
+$(document).ready(function () {
     var topics = ["sports", "basketball", "baseball", "football", "skiing", "swimming", "table tennis",
         "hockey", "golf", "bull riding", "gymnastics", "martial arts", "racing", "cycling"];
     console.log(topics);
@@ -10,46 +11,58 @@ $(document).ready(function () {
     });
 
     function displayGif() {
-
         var sports = $(this).attr("type-sports");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-            sports + "&api_key=RBG2FZTkXcKXAkOSnsX2B1SQR6CsfgeW&limit=10&offset=0&rating=G&lang=en";
+        var queryURL = "https://api.giphy.com/v1/gifs/random?tag=" +
+            sports + "&api_key=RBG2FZTkXcKXAkOSnsX2B1SQR6CsfgeW&rating=G";
         console.log(this);
 
         //   AJAX call
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
+        for (var i = 0; i < 5; i++) {
 
-            var topic = response.data;
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
+                console.log(response);
 
-            for (var i = 0; i < topic.length; i++) {
+                var topic = response.data;
+
                 var sportDiv = $("<div class='card border-dark mb-5' style='width: 20rem'>");
                 var selectImage = $("<img>");
-                var rating = $("<p>").text("Rating: " + topic[i].rating.toUpperCase());
-                var title = $("<p>").text(topic[i].title.toUpperCase());
-                $(selectImage).attr('src', topic[i].images.fixed_height_still.url);
-                $(selectImage).attr('data-still', topic[i].images.fixed_height_still.url);
-                $(selectImage).attr('data-animate', topic[i].images.fixed_height.url);
+                var rating = $("<p>").text("Rating: " + topic.rating.toUpperCase());
+                var title = $("<p>").text(topic.title.toUpperCase());
+                $(selectImage).attr('src', topic.images.fixed_height_still.url);
+                $(selectImage).attr('data-still', topic.images.fixed_height_still.url);
+                $(selectImage).attr('data-animate', topic.images.fixed_height.url);
                 $(selectImage).attr('data-state', 'still');
                 $(selectImage).addClass("gif");
-                
                 $(sportDiv).append(selectImage, title, rating);
                 // $(sportDiv).append(selectImage);
-                $("#display-gif").prepend(sportDiv);               
+                $("#display-gif").prepend(sportDiv);
 
-                // console.log(selectImage);
-                // console.log(title);
-                // console.log(rating);
 
-            }
-        });
+                // for (var i = 0; i < topic.length; i++) {
+                //     var sportDiv = $("<div class='card border-dark mb-5' style='width: 20rem'>");
+                //     var selectImage = $("<img>");
+                //     var rating = $("<p>").text("Rating: " + topic[i].rating.toUpperCase());
+                //     var title = $("<p>").text(topic[i].title.toUpperCase());
+                //     $(selectImage).attr('src', topic[i].images.fixed_height_still.url);
+                //     $(selectImage).attr('data-still', topic[i].images.fixed_height_still.url);
+                //     $(selectImage).attr('data-animate', topic[i].images.fixed_height.url);
+                //     $(selectImage).attr('data-state', 'still');
+                //     $(selectImage).addClass("gif");
+
+                //     $(sportDiv).append(selectImage, title, rating);
+                //     // $(sportDiv).append(selectImage);
+                //     $("#display-gif").prepend(sportDiv);
+                // }
+            });
+        }
+
     }
 
-     // animation control
-     $(document).on("click", ".gif", function () {
+    // animation control
+    $(document).on("click", ".gif", function () {
         var state = $(this).attr("data-state");
         if (state === "still") {
             $(this).attr("src", $(this).attr("data-animate"));
